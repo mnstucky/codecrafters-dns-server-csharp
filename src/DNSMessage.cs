c93dfe03e33fb3c06798bd87b2b7b54ca35887e7
Matt Stucky <59749326+mnstucky@users.noreply.github.com>
2025-02-26 17:58:18 +0000

    Scaffold out header

---
+++ src/DNSMessage.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+
+internal class DNSMessage
+{
+    internal byte[] Message => GetMessage();
+
+    internal byte[] GetMessage()
+    {
+        var bitArray = new BitArray(96);
+        bitArray.SetAll(false);
+        CopyBitArray(_packetIdentifierBits, bitArray, 0);
+        bitArray.Set(17, true);
+        var result = new byte[12];
+        bitArray.CopyTo(result, 0);
+        return result;
+    }
+
+    internal int PacketIdentifier { get; set; } = 1234;
+
+    private BitArray _packetIdentifierBits => new BitArray(BitConverter.GetBytes(PacketIdentifier));
+
+    private static void CopyBitArray(BitArray source, BitArray destination, int startIndex)
+    {
+        if (startIndex + source.Length > destination.Length)
+        {
+            throw new ArgumentException("Destination BitArray is not large enough");
+        }
+
+        for (int i = 0; i < source.Length; i++)
+        {
+            destination[startIndex + i] = source[i];
+        }
+    }
+}
